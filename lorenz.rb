3e2f59f92f8ff4820dc16e243b9cdd5301301e26
Shou Ya <shouyatf@gmail.com>
2013-02-26 02:25:02 +0800

    modified lorenz's equation

--- lorenz.rb
+++ lorenz.rb
@@ -13,31 +13,30 @@ require "rubygame"
 # Use new style events so that this software will work with Rubygame 3.0
 @event_queue.enable_new_style_events
 
+@display_matrix = [[30, 0, 0], [0, 30, 0], [0, 0, 0]]
+@transform_matrix = [@screen.width / 2, @screen.height / 2]
+
 def render_lorenz
-  display_matrix = [[30, 0, 0], [0, 30, 0], [0, 0, 0]]
-  transform_matrix = [@screen.width / 2, @screen.height / 2]
-  x, y, z = 3.051522, 1.582542, 15.62388
+  x, y, z = -1, -2, -3
   dt = 0.0001
 
   sigma, rho, beta = 13, 10, 8.0/3
 
-  200000.times do
+  220000.times do
     x1 = x + sigma * (y - x) * dt
-    y1 = y + (rho * x - x * z - y) * dt
+    y1 = y + (x * (rho - z)) * dt
     z1 = z + (x * y - beta * z) * dt
     x, y, z = x1, y1, z1
 
-#    p [x, y, z]
-
     disp_x = disp_y = nil
-    display_matrix.tap { |m|
+    @display_matrix.tap { |m|
       disp_x = m[0][0] * x + m[0][1] * y + m[0][2] * z
       disp_y = m[1][0] * x + m[1][1] * y + m[1][2] * z
-      disp_x += transform_matrix[0]
-      disp_y += transform_matrix[0]
+      disp_x += @transform_matrix[0]
+      disp_y += @transform_matrix[1]
     }
     @screen.set_at([disp_x, disp_y], [255, 255, 0]) rescue IndexError
-
+    @screen.update
   end
   @screen.update
 end
